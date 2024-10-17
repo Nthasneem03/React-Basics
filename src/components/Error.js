afd03bd05cee72565983934dcbd8ce89df6addda
Naeema Thasneem <150045374+Nthasneem03@users.noreply.github.com>
2024-10-17 13:08:05 +0530

    Update Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,1 +1,14 @@
+import React from 'react'
 
+const Error = () => {
+  return (
+    <>
+    <div className="error">
+        <h1>404</h1>
+        <p>Page Not Found</p>
+    </div>
+    </>
+  )
+}
+
+export default Error
